32a0897ffbbd5e0ec3ed416b84b025d0f08d087c
Ville Skyttä <ville.skytta@iki.fi>
2017-11-01 09:05:58 +0200

    Use parameter expansion instead of basename

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Use this script to test if a given TCP host/port are available
 
-WAITFORIT_cmdname=$(basename $0)
+cmdname=${0##*/}
 
 echoerr() { if [[ $WAITFORIT_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
 
